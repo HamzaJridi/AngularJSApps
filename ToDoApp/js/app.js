3539efe8f189eea9853b880043f31d9704d9ce92
Hamza Jridi <jrid_hamza@yahoo.fr>
2016-04-13 13:04:10 +0100

    add the clearComplete method and some comments

--- ToDoApp/js/app.js
+++ ToDoApp/js/app.js
@@ -4,16 +4,33 @@ ToDo.controller('ToDoCtrl', ['$scope', function($scope) {
 	$scope.todos = [
 		{
 			'title' : 'Build a todo app',
-			'done' : false
+			// set every new task to not done status
+			'done' : false 
 		}
 	];
-
+	//add and display new tasks
 	$scope.addTodo = function(){
+		/*push : adds one or more elts to the end of the array 
+		and returns the new length of the array*/
 		$scope.todos.push({
+			//tie the typed data in the input field with ng-model = newTodo
 			'title' : $scope.newTodo,
 			'done' : false
 		});
+		//disabling the submit btn when the input field is empty
 		$scope.newTodo = ''
 	};
-	$scope.clearCompleted = function(){};
+
+	//delete done tasks from the page
+	$scope.clearCompleted = function(){
+		/* 	*filter() :creates a new array with all elements that pass 
+			the test implemented by the provided function.
+			*this will create a new array with the not done tasks and assign it to
+			the "todos" array
+		*/
+		$scope.todos = $scope.todos.filter(function(item){
+			return !item.done
+		});
+	};
+	
 }]);
