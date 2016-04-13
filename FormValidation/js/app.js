104ccfb6e47d07d08853f4ba110aaf050485aedc
Hamza Jridi <jrid_hamza@yahoo.fr>
2016-04-13 01:05:32 +0100

    working with ngMessage to display

--- FormValidation/js/app.js
+++ FormValidation/js/app.js
@@ -1,7 +1,9 @@
-var myApp = angular.module('myApp', []);
+var myApp = angular.module('myApp', ['ngMessages']);
 
 myApp.controller('RegistrationController', ['$scope', function($scope){
 	$scope.register = function(){
-		$scope.message = 'Welcome ' + $scope.user.firstname; 
+		if ($scope.myform.$valid) {
+			$scope.message = 'Welcome ' + $scope.user.firstname; 
+		}
 	};
 }]);
