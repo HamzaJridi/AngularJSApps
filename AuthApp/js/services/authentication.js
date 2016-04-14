cf078561b5114242bc08f62980ea849a92c76547
Hamza Jridi <jrid_hamza@yahoo.fr>
2016-04-14 15:43:05 +0100

    add an auth service to the app

---
+++ AuthApp/js/services/authentication.js
@@ -0,0 +1,42 @@
+myApp.factory('Authentication',
+    ['$rootscope', '$firebaseAuth','FIREBASE_URL',
+    function($rootscope, $firebaseAuth, FIREBASE_URL){
+
+    }]);//factory
+
+
+
+
+
+
+
+
+myApp.controller('RegistrationController',
+    ['$scope', '$firebaseAuth','FIREBASE_URL',
+        function($scope, $firebaseAuth, FIREBASE_URL){
+            //create an obj to refer to the app's url
+            var ref = new Firebase(FIREBASE_URL);
+            //hold the auth data from firebase
+            var auth = $firebaseAuth(ref);
+
+            $scope.login = function() {
+                $scope.message = "Welcome " + $scope.user.email;
+            };//login
+
+            $scope.register = function() {
+                //create a new user with email and pwd
+                auth.$createUser({
+                    //get data from form's input fields
+                    email: $scope.user.email,
+                    password : $scope.user.password
+                    //usr promise to be sure that firebase has succefully created a user
+                }).then(function(regUser){
+                    $scope.message = "Hi " + $scope.user.firstname + ", Thanks for registering";
+                    //catch an error in case of a used email
+                }).catch(function(error){
+                    $scope.message = error.message;
+
+                })//create user
+            };//register
+        }]);//Controller
+
