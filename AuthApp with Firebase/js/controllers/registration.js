342a264eab1488229d1aa4b67e7bf5303fde7b02
Hamza Jridi <jrid_hamza@yahoo.fr>
2016-04-14 21:11:23 +0100

    adds login for users

--- AuthApp with Firebase/js/controllers/registration.js
+++ AuthApp with Firebase/js/controllers/registration.js
@@ -1,29 +1,13 @@
 myApp.controller('RegistrationController',
-    ['$scope', '$firebaseAuth','FIREBASE_URL',
-        function($scope, $firebaseAuth, FIREBASE_URL){
-            //create an obj to refer to the app's url
-            var ref = new Firebase(FIREBASE_URL);
-            //hold the auth data from firebase
-            var auth = $firebaseAuth(ref);
+    ['$scope', 'Authentication',
+        function($scope, Authentication){
 
             $scope.login = function() {
-                $scope.message = "Welcome " + $scope.user.email;
-        };//login
+                Authentication.login($scope.user);
+        };//login method from Auth service
 
         $scope.register = function() {
-            //create a new user with email and pwd
-            auth.$createUser({
-                //get data from form's input fields
-                email: $scope.user.email,
-                password : $scope.user.password
-            //usr promise to be sure that firebase has succefully created a user
-            }).then(function(regUser){
-                $scope.message = "Hi " + $scope.user.firstname + ", Thanks for registering";
-            //catch an error in case of a used email
-            }).catch(function(error){
-                $scope.message = error.message;
-
-            })//create user
+            Authentication.register($scope.user);
         };//register
 }]);//Controller
 
--- AuthApp with Firebase/js/services/authentication.js
+++ AuthApp with Firebase/js/services/authentication.js
@@ -1,42 +1,48 @@
 myApp.factory('Authentication',
-    ['$rootscope', '$firebaseAuth','FIREBASE_URL',
-    function($rootscope, $firebaseAuth, FIREBASE_URL){
+    ['$rootScope', '$location', '$firebaseAuth','FIREBASE_URL',
+    function($rootScope, $location, $firebaseAuth, FIREBASE_URL){
+        //create an obj to refer to the app's url
+        var ref = new Firebase(FIREBASE_URL);
+        //hold the auth data from firebase
+        var auth = $firebaseAuth(ref);
 
-    }]);//factory
+        return {
+            //the user param is from the reg ctrller
+            login: function(user){
+                auth.$authWithPassword({
+                    email:user.email,//from the login form
+                    password:user.password
+                }).then(function(regUser){
+                    //redirect to succes page
+                    $location.path('/success');
+                }).catch(function(error) {
+                    $rootScope.message = error.message ;
+                });
+            },//login
 
-
-
-
-
-
-
-
-myApp.controller('RegistrationController',
-    ['$scope', '$firebaseAuth','FIREBASE_URL',
-        function($scope, $firebaseAuth, FIREBASE_URL){
-            //create an obj to refer to the app's url
-            var ref = new Firebase(FIREBASE_URL);
-            //hold the auth data from firebase
-            var auth = $firebaseAuth(ref);
-
-            $scope.login = function() {
-                $scope.message = "Welcome " + $scope.user.email;
-            };//login
-
-            $scope.register = function() {
+            register:function(user) {
                 //create a new user with email and pwd
                 auth.$createUser({
                     //get data from form's input fields
-                    email: $scope.user.email,
-                    password : $scope.user.password
+                    email: user.email,
+                    password : user.password
                     //usr promise to be sure that firebase has succefully created a user
                 }).then(function(regUser){
-                    $scope.message = "Hi " + $scope.user.firstname + ", Thanks for registering";
+                    var regRef = new Firebase(FIREBASE_URL + 'users')
+                        .child(regUser.uid).set({
+                            date: Firebase.ServerValue.TIMESTAMP,
+                            regUser: regUser.uid,
+                            firstname: user.firstname,//from the register form
+                            lastname: user.lastname,
+                            email:user.email
+                        });//user infos
+
+                    $rootScope.message = "Hi " + user.firstname + ", Thanks for registering";
                     //catch an error in case of a used email
                 }).catch(function(error){
-                    $scope.message = error.message;
+                    $rootScope.message = error.message;
 
                 })//create user
-            };//register
-        }]);//Controller
-
+            }//register
+        };
+    }]);//factory
