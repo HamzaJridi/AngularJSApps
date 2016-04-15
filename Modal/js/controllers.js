71d32b28e22b4e31069f8b8e7cf6459c57748fe8
Hamza Jridi <jrid_hamza@yahoo.fr>
2016-04-15 16:35:27 +0100

    add controller and css styles

--- Modal/js/controllers.js
+++ Modal/js/controllers.js
@@ -1,5 +1,8 @@
+var myApp = angular.module('myApp', []);
+
 myApp.controller('ModalCtrl', ['$scope', function($scope) {
     $scope.showModal = false;
+    $scope.errormsg = "Please type a valid email and pwd"
     $scope.toggleModal = function() {
         $scope.showModal = !$scope.showModal;
     };
@@ -7,6 +10,42 @@ myApp.controller('ModalCtrl', ['$scope', function($scope) {
 
 myApp.directive('modal', function() {
    return {
+       template: '<div class="modal fade">' +
+       '<div class="modal-dialog">' +
+       '<div class="modal-content">' +
+       '<div class="modal-header">' +
+       '<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>' +
+       '<h4 class="modal-title">{{ title }}</h4>' +
+       '</div>' +
+       '<div class="modal-body" ng-transclude></div>' +
+       '</div>' +
+       '</div>' +
+       '</div>',
+       restrict: 'E',
+       transclude: true,
+       replace:true,
+       scope:true,
+       link: function postLink(scope, element, attrs) {
+           scope.title = attrs.title;
 
+           scope.$watch(attrs.visible, function(value){
+               if(value == true)
+                   $(element).modal('show');
+               else
+                   $(element).modal('hide');
+           });
+
+           $(element).on('shown.bs.modal', function(){
+               scope.$apply(function(){
+                   scope.$parent[attrs.visible] = true;
+               });
+           });
+
+           $(element).on('hidden.bs.modal', function(){
+               scope.$apply(function(){
+                   scope.$parent[attrs.visible] = false;
+               });
+           });
+       }
    };
 });
