aaeb30873e8cbb29a82e78f3ed05c5fefadd7d35
Hamza Jridi <jrid_hamza@yahoo.fr>
2016-04-15 01:45:43 +0100

    set login method for multiple users stored in a json file

--- AuthApp(frontEnd)/js/controllers/registration.js
+++ AuthApp(frontEnd)/js/controllers/registration.js
@@ -2,14 +2,24 @@ myApp.controller('LoginCtrl',
     ['$scope', '$http', '$location',
     function($scope, $http, $location) {
         $http.get('js/data.json').success(function(data) {
-           //$scope.data= data;
+           $scope.data= data;
            $scope.log=function(){
-                if($scope.user.name=='hamza'&& $scope.user.password=='hamza'){
-                    $location.path('/home');
-                }else {
-                    alert('login incorrect');
-                }
+               var j=0;
+               for (var i =0; i < $scope.data.length; i++) {
+
+                   if($scope.user.name==$scope.data[i].name && $scope.user.password==$scope.data[i].password) {
+                       j++;
+                   }
+               }
+               if (j>0) {
+                   $location.path('/home');
+               } else {
+                   alert('login incorrect')
+                   $scope.user.name = '';
+                   $scope.user.password = '';
+               }
+
             };
+
         });
     }]);
-
